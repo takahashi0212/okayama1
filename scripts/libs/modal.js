ade895f114e7fd2e9d23c5d9250a12ea0dc4338d
takahashi0212 <69074832+takahashi0212@users.noreply.github.com>
2020-12-01 22:45:46 +0900

    Add files via upload

--- scripts/libs/modal.js
+++ scripts/libs/modal.js
@@ -2,12 +2,19 @@
 
 {
   const open = document.getElementById('open');
+  const open02 = document.getElementById('open02');
   const close = document.getElementById('close');
+  const close02 = document.getElementById('close02');
   const modal = document.getElementById('modal');
+  const modal02 = document.getElementById('modal02');
   const mask = document.getElementById('mask');
 
   open.addEventListener('click', () => {
     modal.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+  open02.addEventListener('click', () => {
+    modal02.classList.remove('hidden');
     mask.classList.remove('hidden');
   });
 
@@ -16,9 +23,15 @@
     mask.classList.add('hidden');
   });
 
+  close02.addEventListener('click', () => {
+    modal02.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+
   mask.addEventListener('click', () => {
     // modal.classList.add('hidden');
     // mask.classList.add('hidden');
     close.click();
+    close02.click();
   });
 }
