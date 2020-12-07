a97489ee9f4fa5994ad5c79bafbf3565e329b678
takahashi0212 <69074832+takahashi0212@users.noreply.github.com>
2020-12-08 00:58:44 +0900

    Add files via upload

---
+++ okayama/scripts/libs/modal.js
@@ -0,0 +1,37 @@
+'use strict';
+
+  const open = document.getElementById('open');
+  const open02 = document.getElementById('open02');
+  const close = document.getElementById('close');
+  const close02 = document.getElementById('close02');
+  const modal = document.getElementById('modal');
+  const modal02 = document.getElementById('modal02');
+  const mask = document.getElementById('mask');
+
+  open.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    mask.classList.remove('hidden');
+    console.log('クリック');
+  });
+  open02.addEventListener('click', () => {
+    modal02.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+
+  close.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+
+  close02.addEventListener('click', () => {
+    modal02.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+
+  mask.addEventListener('click', () => {
+    // modal.classList.add('hidden');
+    // mask.classList.add('hidden');
+    close.click();
+    close02.click();
+  });
+
